ba279ed628aa7f1fa6cb7f0dd85f52b196bceba7
Imran Fazilov <ifazilov3584@bths.edu>
2023-01-17 12:28:56 -0500

    setup

--- src/WinCondition.java
+++ src/WinCondition.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class WinCondition {
+public class WinCondition
+{
+    private int[] winningSpaces;
+    private String hey;
+
+    public WinCondition(int space1, int space2, int space3)
+    {
+        winningSpaces = new int[3];
+        winningSpaces[0] = space1;
+        winningSpaces[1] = space2;
+        winningSpaces[2] = space3;
+    }
+
+    public int[] getWinningSpaces()
+    {
+        return winningSpaces;
+    }
 }
